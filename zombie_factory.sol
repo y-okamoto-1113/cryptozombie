5839dfb293b3807ee5d8eeabda1418336b6d2c09
y-okamoto-1113 <okamoto.yuuki.1113+github@gmail.com>
2023-03-18 22:44:35 +0900

    :tada: Created Zombie Factory

---
+++ zombie_factory.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.17;
+
+contract ZombieFactory {
+    // イベントをここで宣言するのだ
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    function _createZombie(string memory _name, uint _dna) private {
+        zombies.push(Zombie(_name, _dna));
+        uint id = zombies.length;
+        // ここでイベントを発生させるのだ
+        emit NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(
+        string memory _str
+    ) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+}
