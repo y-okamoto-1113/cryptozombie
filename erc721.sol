796af00d2e25c84c5a7513206bb27dd83bed360a
y-okamoto-1113 <okamoto.yuuki.1113+github@gmail.com>
2023-03-25 01:37:14 +0900

    :arrow_up: Updated solidity from `0.4.19` to `0.8.17`

---
+++ erc721.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+interface ERC721 {
+  event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
+  event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
+
+  function balanceOf(address _owner) external view returns (uint256 _balance);
+  function ownerOf(uint256 _tokenId) external view returns (address _owner);
+  function transfer(address _to, uint256 _tokenId) external;
+  function approve(address _to, uint256 _tokenId) external;
+  function takeOwnership(uint256 _tokenId) external;
+}
